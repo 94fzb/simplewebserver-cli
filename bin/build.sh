9955c78d9b4d5c8e97bddae241011a941215a693
xiaochun <xchun90@163.com>
2017-07-09 18:09:43 +0800

    change final file path

--- bin/build.sh
+++ bin/build.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
+mkdir zip
 mvn clean package
 cp bin/simplewebserver.sh simplewebserver.sh
 chmod a+x simplewebserver.sh
 zip -r jlink/target/*.zip simplewebserver.sh
-cp jlink/target/*.zip  simplewebserver.cli-linx-x64-0.0.6.zip
+cp jlink/target/jlink*.zip zip/simplewebserver.cli-linx-x64-0.0.6.zip
 rm simplewebserver.sh
